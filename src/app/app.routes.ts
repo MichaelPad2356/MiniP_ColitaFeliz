da1edcb49d4ae94311197845959dcf144c17c972
unknown <gm045102@gmail.com>
2024-05-02 16:25:45 -0600

    opcion buscar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdopcionesComponent } from './adopciones/adopciones.component';
 import { DatosCitasComponent } from './datos-citas/datos-citas.component';
+import { BuscarComponent } from './buscar/buscar.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'adopciones', component: AdopcionesComponent},
-    {path: 'citas/:index', component: DatosCitasComponent}
+    {path: 'citas/:index', component: DatosCitasComponent},
+    {path: 'buscar', component: BuscarComponent}
 ];
