3e5dc096aa2a70f716394eef7a6dbaff5b51079e
AketzaliCastorena <aketzalicastorena18@gmail.com>
2024-05-02 18:22:17 -0600

    se modificaron cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdopcionComponent } from './adopcion/adopcion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AdopcionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
