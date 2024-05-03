384e1f4de3bc2315fc78d3e920f61343d8be6092
AketzaliCastorena <aketzalicastorena18@gmail.com>
2024-05-02 18:23:05 -0600

    Componente donacion

---
+++ src/app/donacion/donacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './donacion.component.html',
+  styleUrl: './donacion.component.css'
+})
+export class DonacionComponent {
+
+}
