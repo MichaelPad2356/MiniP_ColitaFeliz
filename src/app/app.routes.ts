4f8c3c764b4b2728b0fb95b45c171109044b7cc6
MichaelPad2356 <maicgio123@gmail.com>
2024-05-02 20:57:54 -0600

    Correcciones en Nosotros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { AdopcionesComponent } from './adopciones/adopciones.component';
 import { DatosCitasComponent } from './datos-citas/datos-citas.component';
 import { BuscarComponent } from './buscar/buscar.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'adopciones', component: AdopcionesComponent},
     {path: 'citas/:index', component: DatosCitasComponent},
     {path: 'buscar', component: BuscarComponent},
+    {path: 'nosotros', component: NosotrosComponent},
     {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
