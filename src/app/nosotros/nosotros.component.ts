4498bdd479d62e58c07f7f817c8a6588be91d741
Michh13 <michelleg1302@gmail.com>
2024-05-02 23:51:12 -0600

    se agregaron cambios para poder agregar el video con pipes, aun sin terminar

--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-nosotros',
   standalone: true,
   imports: [],
   templateUrl: './nosotros.component.html',
-  styleUrl: './nosotros.component.css'
+  styleUrls: ['./nosotros.component.css']
 })
-export class NosotrosComponent {
+export class NosotrosComponent implements OnInit {
+  videoUrl: SafeResourceUrl | null = null; // Inicializamos a null
 
+  constructor(private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    const videoUrlString = 'https://www.youtube.com/watch?v=zQ6CB4pZIYs';
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(videoUrlString);
+  }
 }
