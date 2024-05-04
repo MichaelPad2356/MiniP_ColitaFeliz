c0ff85582fe4d1d9ddf038c6ab227f618d3d6cd3
AketzaliCastorena <aketzalicastorena18@gmail.com>
2024-05-03 19:02:32 -0600

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { AdopcionComponent } from './adopcion/adopcion.component';
+import { DonacionComponent } from './donacion/donacion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, MenuComponent, HomeComponent, FooterComponent],
+  imports: [RouterOutlet, RouterModule, MenuComponent, HomeComponent, FooterComponent,AdopcionComponent,DonacionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
